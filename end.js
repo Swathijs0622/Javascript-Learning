dd9b826cc40e782de2b09cb018f3797380fcf370
Swathi J S <swathigowda0401@gmail.com>
2022-11-29 22:15:02 +0530

    Saved high scores in localstorage

--- end.js
+++ end.js
@@ -3,6 +3,10 @@ const saveScorebtn = document.getElementById('saveScoreBtn');
 const finalScore = localStorage.getItem('finalScore');
 const mostRecentScore = localStorage.getItem('mostrecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -10,7 +14,17 @@ username.addEventListener("keyup", () => {
     saveScorebtn.disabled = !username.value;
 });
 
-saveHighScore = (e) => {
+let saveHighScore = (e) => {
     e.preventDefault();
 
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 };
